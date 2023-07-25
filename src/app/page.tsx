3478462591172f82194562e5e15fa4635e265492
igormaxlima <igormaxnunes@gmail.com>
2023-07-25 19:57:41 -0300

    criando arquivo para o navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { TextField, Button } from '@mui/material'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center py-5'>
+    <div>
+        <Navbar />
 
-      <TextField
-        label='Nome da Sala'
-        variant='outlined'
-      />
-      <Button variant="contained">Entrar</Button>
+      <div className='flex justify-center items-center py-5'>
+        <TextField
+          label='Nome da Sala'
+          variant='outlined'
+        />
+        <Button variant="contained">Entrar</Button>
+      </div>
 
     </div>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div>
+        <h1>
+          NAVBAR
+        </h1>
+    </div>
+  )
+;}
