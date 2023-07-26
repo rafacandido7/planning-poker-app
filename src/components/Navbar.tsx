5662ee4bb81ad1058240e1d436eadc064590c843
Rafael Candido <rafacandido0708@gmail.com>
2023-07-26 10:48:07 -0300

    fix(build)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import Logo from '../../public/assets/logo.jpg'
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center items-center bg-custom p-5">
+    <div className='flex justify-center items-center bg-custom p-5'>
       <Image
         src={Logo}
         width={100}
-        alt="Logo da marca"
-        className="absolute left-0"
+        alt='Logo da marca'
+        className='absolute left-0'
       />
-      <h1 className="text-white font-bold uppercase text-2xl">
+      <h1 className='text-white font-bold uppercase text-2xl'>
         Planning Poker
       </h1>
     </div>
