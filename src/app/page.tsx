000cf801667820a8fb735842acc07b4cfbfee81f
igormaxlima <igormaxnunes@gmail.com>
2023-07-25 21:28:32 -0300

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
 import { TextField, Button } from '@mui/material'
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+import Banner from '../../public/assets/banner.jpg'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-primary'>
         <Navbar />
 
-      <div className='flex justify-center items-center py-5'>
+      <div className='flex justify-center items-center py-10'>
         <TextField
           label='Nome da Sala'
-          variant='outlined'
+          variant="standard"
+          id="standard-basic"
+          className='mr-5'
         />
-        <Button variant="contained">Entrar</Button>
+        <Button variant="outlined">Entrar</Button>
+
+      </div>
+
+      <div className='flex justify-center'>
+        <Image
+          src={Banner}
+          alt="Foto de um Planning Poker"
+          width={600}
+
+        />
       </div>
 
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <div>
-        <h1>
-          NAVBAR
+    <div className="flex justify-center items-center bg-custom p-5">
+        <h1 className="text-white font-bold uppercase text-2xl">
+          Planning Poker
         </h1>
     </div>
   )
